97309fb247b7b54197099e004ef534300c994a3b
kiramck24-ship-it <kiramck24@gmail.com>
2026-09-22 19:47:53 -0500

    udpated files

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,16 +1,27 @@
+
 package tests;
+
+import org.testng.annotations.Test;
+
 import base.BaseTest;
 import pages.LoginPage;
 import pages.ProfilePage;
+
 public class LoginTest extends BaseTest {
-    public static void main(String[] args) throws InterruptedException {
-        LoginTest test = new LoginTest();
-        test.setUp();
-        LoginPage login = new LoginPage(test.page);
-        login.login("TestUser", "Test@123");
-         ProfilePage profile = new ProfilePage(test.page);
-         System.out.println(profile.getPageTitle());
-        System.out.println(profile.isLogoutDisplayed());
-                test.tearDown();
-    }
+
+@Test
+public void loginTest() {
+
+setUp();
+
+LoginPage login = new LoginPage(page);
+login.login("TestUser", "Test@123");
+
+ProfilePage profile = new ProfilePage(page);
+
+System.out.println(profile.getPageTitle());
+System.out.println(profile.isLogoutDisplayed());
+
+tearDown();
+}
 }
--- src/test/java/tests/TextBoxTest.java
+++ src/test/java/tests/TextBoxTest.java
@@ -1,3 +1,4 @@
+
 package tests;
 import base.BaseTest;
 import pages.LoginPage;
@@ -5,23 +6,24 @@ import pages.ProfilePage;
 import pages.TextboxPage;
 
 public class TextBoxTest extends BaseTest{
-	 public static void main(String[] args) {
-	     LoginTest test = new LoginTest();
-	        test.setUp();
-	        LoginPage login = new LoginPage(test.page);
-	        login.login("TestUser", "Test@123");
-	        ProfilePage profile = new ProfilePage(test.page);
-	        profile.clickElement();
-	        profile.clickTextBox();
-	        TextboxPage TextboxPage = new TextboxPage(test.page);
-	        TextboxPage.enterFullname("Komal");
-	        TextboxPage.enterEmail("komal@gmail.com");
-	            
-	        TextboxPage.submitbutton();
-	       
-	     // Assertions
-		      TextboxPage.verifyFullnameDisplayed("Komal");
-		       
-	        test.tearDown();
-	 }
+public static void main(String[] args) {
+LoginTest test = new LoginTest();
+test.setUp();
+LoginPage login = new LoginPage(test.page);
+login.login("TestUser", "Test@123");
+ProfilePage profile = new ProfilePage(test.page);
+profile.clickElement();
+profile.clickTextBox();
+TextboxPage textboxpage = new TextboxPage(test.page);
+textboxpage.enterFullname("Komal");
+textboxpage.enterEmail("komal@gmail.com");
+
+// page.waitForTimeout(10000);
+textboxpage.submitbutton();
+
+// Assertions
+textboxpage.verifyFullnameDisplayed("Komal");
+
+test.tearDown();
+}
 }
